1fd2a1786e46610ed7a71d776c82d18c64f0e5b6
Ana Caroline Moraes <166078471+Anymoraes5@users.noreply.github.com>
2024-11-15 23:01:22 -0300

    [feat] sidebar e responsividade

--- JS/script.js
+++ JS/script.js
@@ -5,14 +5,25 @@ fetch("header.html")
         // Insere o conteúdo carregado no lugar do "header-placeholder"
         document.getElementById("header-placeholder").innerHTML = data;
         
-        // Preenche dinamicamente a saudação, caso precise
-        document.getElementById("saudacao").textContent = "Bem-vindo, Usuário!";
     })
     .catch(error => {
         // Em caso de erro ao carregar o cabeçalho
         console.error("Erro ao carregar o cabeçalho:", error);
     });
- // Função de login (simples, apenas para exemplo)
+
+// Carrega o conteúdo do cabeçalho
+fetch("sidebar.html")
+    .then(response => response.text())  // Pega o conteúdo do arquivo header.html
+    .then(data => {
+        // Insere o conteúdo carregado no lugar do "header-placeholder"
+        document.getElementById("side-bar").innerHTML = data;
+        
+    })
+    .catch(error => {
+        // Em caso de erro ao carregar o cabeçalho
+        console.error("Erro ao carregar o cabeçalho:", error);
+    });
+ //Função de login (simples, apenas para exemplo)
  function login(event) {
     event.preventDefault(); // Impede o envio do formulário
 
@@ -28,3 +39,32 @@ fetch("header.html")
         alert("Credenciais inválidas");
     }
 }
+
+$(document).ready(function(){
+    $('.login-info-box').fadeOut();
+    $('.login-show').addClass('show-log-panel');
+});
+
+//Saudação
+// Função para exibir a saudação com base na hora atual
+function exibirSaudacao() {
+    const saudacaoElemento = document.getElementById("saudacao"); // Seleciona o elemento HTML
+    const horaAtual = new Date().getHours(); // Obtém a hora atual (0-23)
+
+    let saudacaoTexto;
+
+    // Define a saudação com base no horário
+    if (horaAtual >= 0 && horaAtual < 12) {
+        saudacaoTexto = "Bom dia!";
+    } else if (horaAtual >= 12 && horaAtual < 18) {
+        saudacaoTexto = "Boa tarde!";
+    } else {
+        saudacaoTexto = "Boa noite!";
+    }
+
+    // Atualiza o texto do elemento com a saudação
+    saudacaoElemento.textContent = saudacaoTexto;
+}
+
+// Executa a função ao carregar a página
+window.onload = exibirSaudacao;
